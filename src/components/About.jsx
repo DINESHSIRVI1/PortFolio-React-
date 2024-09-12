b9a7f611eaee12e1bcf5231c14d36cf51de6985d
DINESHSIRVI1 <114683449+DINESHSIRVI1@users.noreply.github.com>
2024-09-12 22:51:18 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className=" text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Dinesh Sirvi, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          GOVT. HR. SEC. SCHOOL,BALI Afflicated to RBSC 
+          Senior Secondary (89.00%)     
+          <br />              
+          HULASIBAI BASTIMAL GOVT SEC SCH, BALI                                                                                                                             
+          Secondary       (74.00%)      
+          <br />                                                                                                                                                       
+          [B.Tech/Introduction to MongoDB,Celebal Summer Internship 2024 in React.js (CT_CSI_RJ_1899),
+          <br />
+          Tailwind CSS on Udemy(UC-02b1d4aa-c142-4443-81c6-3f63510ed689)], [Institution:RTU], [Year:2025],
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in [Programming Languages:JavaScript,java,c ] Experienced with [Software
+          Tools:VScode] Strong grasp of [Design:UI]
+          Excellent problem-solving skills Effective communicator and
+          collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          [Job Title], [Company/Organization], [Dates] [Brief description of
+          responsibilities and achievements] [Job Title],
+          [Company/Organization], [Dates] [Brief description of responsibilities
+          and achievements] [Freelance/Contract Work], [Client/Organization],
+          [Dates] [Brief description of projects and contributions]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          [Award/Recognition], [Organization/Institution], [Year] [Achievement],
+          [Organization/Platform], [Year]
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative in computerscience solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
